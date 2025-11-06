d305bba719d5629bad30a170f3afc3700e597e07
Jaime Evalle <jaime@sabreiw.com>
2025-11-06 15:36:37 +0800

    added the fronend dist

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -14,7 +14,7 @@ let notes = [
   },
   {
     id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol!',
+    content: 'GET and POST are the most important methods of HTTP protocol!!',
     important: true,
   },
   
@@ -33,6 +33,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
